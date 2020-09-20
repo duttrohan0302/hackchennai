151c6e4e44e14648074b00d4414ead7fadb610ac
Rohan Dutt <duttrohan0302@gmail.com>
2020-09-20 10:23:08 +0530

    packages and default routing

--- frontend/src/Routing/Routes.js
+++ frontend/src/Routing/Routes.js
@@ -3,15 +3,18 @@ import { Switch } from 'react-router-dom';
 import HomePage from '../Components/HomePage';
 import Districts from '../Components/Districts';
 import DashBoard from '../Components/DashBoard';
+import Login from '../Components/Auth/Login';
 
 import DefaultLayoutRoute from './DefaultLayout';
+import PrivateRoute from './PrivateRoute';
 import ParticularDistrict from '../Components/ParticularDistrict';
 
 
 const Routes =  ()  => {
   return (
       <Switch>
-        <DefaultLayoutRoute exact path="/" component={HomePage} />
+        <PrivateRoute exact path="/" component={HomePage} />
+        <DefaultLayoutRoute exact path="/login" component={Login} />
         <DefaultLayoutRoute exact path="/allDistricts" component={Districts} />
         <DefaultLayoutRoute exact path="/dashboard" component={DashBoard} />
         <DefaultLayoutRoute exact path="/district/:name" component={ParticularDistrict} />
