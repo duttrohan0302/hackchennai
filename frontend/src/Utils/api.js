27ff4c6e0c425caa6e3a01205cff6dc9268f2d63
Rohan Dutt <duttrohan0302@gmail.com>
2020-09-20 10:21:42 +0530

    api utilities and auth token setup

--- frontend/src/Utils/api.js
+++ frontend/src/Utils/api.js
@@ -1,8 +1,28 @@
 import axios from 'axios';
+import store from '../Helpers/store';
+import { LOGOUT } from '../Actions/types';
 
-export const api = axios.create({
+const api = axios.create({
   baseURL: 'http://localhost:5000',
   headers: {
     'Content-Type': 'application/json'
   }
 });
+/**
+ intercept any error responses from the api
+ and check if the token is no longer valid.
+ ie. Token has expired
+ logout the user if the token has expired
+**/
+
+api.interceptors.response.use(
+  res => res,
+  err => {
+    if (err.response.data.msg === 'Token is not valid') {
+      store.dispatch({ type: LOGOUT });
+    }
+    return Promise.reject(err);
+  }
+);
+
+export default api;
